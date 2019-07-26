4450ea188a53e993f3babde57ab42246dbbc2a85
EC2 Default User <ec2-user@ip-172-31-16-144.us-east-2.compute.internal>
2019-07-26 14:29:32 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+def hello
+  render html:"¡hola,mundo!"
+end
+
+def goodby
+  render html:"goodby,world"
+end
+end
